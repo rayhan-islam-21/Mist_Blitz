ffe0e25a1e2cf84b0ed986c836232886b09ab601
Rayhan Islam <merayhanislam21@gmail.com>
2025-12-24 23:47:05 +0600

    final push

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './dashboard/page';
+
+const layout = () => {
+    return (
+        <div>
+                <Dashboard/>
+        </div>
+    );
+};
+
+export default layout;
