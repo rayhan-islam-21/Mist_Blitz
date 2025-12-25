8c1f526a04751fb1c7f5e5aaa8e2922c6462dd40
Rayhan Islam <merayhanislam21@gmail.com>
2025-12-25 20:55:47 +0600

    final push before forwarding

--- src/app/api/members/route.js
+++ src/app/api/members/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
-import member from "@/model/member";
+import Member from "@/model/member";
+
 
 export async function POST(req) {
   try {
@@ -11,12 +12,12 @@ export async function POST(req) {
     console.log("Incoming body:", body);
 
 
-    const existingUser = await member.findOne({ roll });
+    const existingUser = await Member.findOne({ roll });
     if (existingUser) {
       return NextResponse.json({ message: "User already exists" }, { status: 409 });
     }
 
-    const newUser = await member.create({ name, roll, dept, image, linkedin, position });
+    const newUser = await Member.create({ name, roll, dept, image, linkedin, position });
     return NextResponse.json(newUser, { status: 201 });
   } catch (error) {
     console.error("Error creating user:", error);
@@ -29,7 +30,7 @@ export async function GET() {
   try {
     await connectDB();
 
-    const users = await member.find(); 
+    const users = await Member.find(); 
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
     console.error("Error fetching users:", error);
