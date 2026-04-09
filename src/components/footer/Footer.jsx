427728ab869025f2be428caaed98e9b8addf00f3
Rayhan Islam <merayhanislam21@gmail.com>
2026-04-09 23:20:28 +0600

    ui imporved

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -90,7 +90,7 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 body-font">
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-12 mb-12">
           {/* Brand section */}
           <div className="lg:col-span-1">
